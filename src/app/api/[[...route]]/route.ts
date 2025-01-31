a18157ab26d29550ed9f8254c2132ca91405d562
M-Taguchi <masaki.taguchi@rakus.co.jp>
2025-01-31 09:10:43 +0900

    feat: dbセットアップ

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,28 @@
+import { D1Database } from '@cloudflare/workers-types';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB: D1Database;
+    }
+  }
+}
+
+type Bindings = {
+    DB: D1Database;
+}
+
+const app = new Hono<{ Bindings: Bindings}>().basePath('/api')
+
+app.get("/query/customers", async (c) => {
+    const { results } = await process.env.DB.prepare("SELECT * FROM customers").all()
+    return c.json(results)
+})
 
 app.get('/hello', (c) => {
   return c.json({
